32ec85ab7fce39a34293e11ef3d9f57adf8baaa2
jwolrai <jwolrai@outlook.com>
2018-10-27 19:01:01 -0700

    created 4th game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 // memory game ga
 
-var cards =  ['queen','queen','king','king'];
+var cards =  [
+    {
+        rank:'queen',
+        suit:'hearts',
+        cardImage:'images/queen-of-hearts.png'
+    },
+    {
+        rank:'queen',
+        suit:'diamonds',
+        cardImage:'images/queen-of-diamonds.png'
+    },
+    {
+        rank:'king',
+        suit:'hearts',
+        cardImage:'images/king-of-hearts.png'
+    },
+    {
+        rank:'king',
+        suit:'diamonds',
+        cardImage:'images/queen-of-diamonds.png'
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,7 +32,8 @@ var checkForMatch = function(){
     }
 }
 var flipCard = function(cardId){
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
 
 }
 flipCard(0);
