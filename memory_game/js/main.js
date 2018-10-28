90abedca54daf1194d7e3bbff423cfdc5cb9e5ed
jwolrai <jwolrai@outlook.com>
2018-10-27 18:55:33 -0700

    created 3rd game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,17 @@
 var cards =  ['queen','queen','king','king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0]; 
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if(cardsInPlay.length === 2){
-    if(cardsInPlay[0]==cardsInPlay[1]){
-        alert('you found a match');
+var checkForMatch = function(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
     }
 }
-var cardThree = "king";
-var cardFour = "king";
+var flipCard = function(cardId){
+    cardsInPlay.push(cards[cardId]);
 
-
-
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
